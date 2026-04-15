98d9d7c909996891be8b87dbbfcd8c17c5788cc1
tajuddin033 <tajuddinjunaid0@gmail.com>
2026-04-15 15:57:23 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,7 @@
+a,b,c=input("enter three integer number:").split()
+if a>b and a>c:
+    print("largest of three numbers:",a)
+if b>c and b>a:
+    print ("largest of three numbers:",b)
+if c>a and c>b:
+    print ("largest of three numbers:",c)
